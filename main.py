e45eb116482d41b4d4c3e680b40d6ce4e9841286
AzatSardaryan <azat.sardaryan1@hotmail.com>
2024-09-12 17:51:11 +0100

    Completed Scraper

--- main.py
+++ main.py
@@ -8,11 +8,7 @@ all_teams = []
 url = 'https://fbref.com/en/comps/9/2023-2024/2023-2024-Premier-League-Stats'
 
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-}
-
-html = requests.get(url, headers=headers).text
+html = requests.get(url).text
 
 
 soup = BeautifulSoup(html, "lxml")
@@ -25,4 +21,17 @@ links = [l for l in links if '/squads' in l]
 
 team_urls = [f"https://fbref.com{l}" for l in links]
 
-print(team_urls)
+for team_url in team_urls:
+    team_name = team_url.split("/")[-1].replace("-Stats", "")
+    data = requests.get(team_url).text
+    soup = BeautifulSoup(data, "lxml")
+    stats = soup.find_all("table", class_ = "stats_table")[0]
+
+    if stats and stats.columns: stats.columns = stats.columns.droplevel()
+    team_data = pd.read_html(str(stats))[0]
+    team_data["Team"] = team_name
+    all_teams.append(team_data)
+    time.sleep(5)
+
+stat_df = pd.concat(all_teams)
+stat_df.to_csv("stats.csv")
